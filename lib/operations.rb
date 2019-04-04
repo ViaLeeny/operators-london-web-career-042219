66c8b6dab70891ccf32abcf9fb7328876990f12f
Ayleen  Nazario <ayleen.nazario@gmail.com>
2019-04-04 00:12:10 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,14 @@
+require pry
 def unsafe?(speed)
-
+if speed < 40 || speed > 60
+  binding.pry
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60 ?
+	  binding.pry
 end
 	
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-require_relative '../lib/operations.rb'
+require_relative '.operations_spec.rb'
+require_relative '..operations.rb'
+
 
 RSpec.configure do |config|
-  # config here
+
+
 end
